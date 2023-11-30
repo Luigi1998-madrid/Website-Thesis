8908309b62b834342c2f30f4cb0a48b7bf05eaee
Luigi1998-madrid <antoniello.luigi98@gmail.com>
2023-11-30 13:55:45 +0100

    30/11/2023

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -3,3 +3,17 @@ function show(){
 	panel1.style.display = "block";
 
 }
+
+function active_canvas  (ide) {
+    // alert("hola");
+    var padre = document.getElementById("contnt");
+    var elementosHijosDiv = padre.children;
+
+    for (var i = 0; i < elementosHijosDiv.length; i++) {
+           elementosHijosDiv[i].style.display = "none";
+           
+    }
+    var elemento = document.getElementById(ide);
+    elemento.style.display="block"
+    
+}
